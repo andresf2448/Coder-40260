118c5535f6fffc9eb22cd493c84338c8588787ec
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-13 14:02:06 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,60 @@
+//comentario de linea
+
+/* 
+comentario
+de 
+bloque
+*/
+
+/* //Declaracion de variable
+let nombre;
+
+//Definicion de variable o asignacion
+nombre = "andres";
+
+let edad = 5; */
+
+/* const EDAD = 5;
+
+let numeroUnoBonito */
+
+/* let numeroUno = 3;
+let numeroDos = 7;
+
+let suma = numeroUno + numeroDos;
+console.log(suma); */
+
+/* let saludo = "Hola";
+let nombre = "Gonza";
+let mensaje = saludo + " " + nombre;
+
+console.log("============================================", mensaje); */
+
+/* let edad = 10;
+let nombre = "andres";
+
+console.log("Bienvenido senor " + nombre + " tienes " + edad + " de edad"); */
+
+/* let nombre = prompt("ingresa el nombre");
+console.log("Hola " + nombre); */
+/* let nombre = "andres";
+let edad = 8;
+console.log(typeof(edad)); */
+
+/* let nombre = prompt("ingresa el nombre");
+let edad = prompt("Ingrese la edad");
+
+let mensaje = "Bienvenid@ " + nombre + " tienes " + edad + " de edad";
+
+alert(mensaje); */
+
+/* let numeroUno = parseInt(prompt("ingrese el número uno"));
+let numeroDos = parseInt(prompt("ingrese el número dos"));
+
+let suma = numeroUno + numeroDos;
+alert(suma); */
+
+let numberOne = prompt('Ingrese el primer numero');
+let numberTwo = prompt('Ingrese el segundo numero');
+let suma = parseInt(numberOne) + parseInt(numberTwo);
+alert(suma);
