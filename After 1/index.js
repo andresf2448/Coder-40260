e7c62de7c209b9f95475922795778d611716c13b
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-22 17:44:12 -0500

    After 1

---
+++ After 1/index.js
@@ -0,0 +1,43 @@
+/* let nombre = "Felipe";
+let numero = 11;
+const EDAD = 15;
+
+if(numero === 7){
+  console.log("Eres el 7");
+}else if(numero === 10){
+  console.log("Eres el 10");
+}else{
+  console.log("No eres el 10 ni el 7");
+} */
+
+/* for(let i = 0; i < 100; i++){
+  console.log("Hola");
+} */
+
+/* for(let i = 0; i < 100; i++){
+  if(i === 50){
+    continue;
+  }
+
+  console.log(i);
+}
+
+console.log("terminamos"); */
+/* let valor = true;
+
+while(valor){
+  console.log("hola");
+  valor = false;
+} */
+
+//saber si un numero es par o impar
+
+/* let numero = parseInt(prompt("Ingrese un numero para saber si es par o impar"));
+
+if(numero % 2 === 0){
+  alert("el numero ingresado es par");
+}else{
+  alert("el numero es impar");
+} */
+let edad = 6;
+console.log(`Edad ${edad}`);
