2d7b715fca907c3611265aad11facd7c75e054fd
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-09-03 11:26:15 -0500

    Clase 4

--- Clase 4/bloque1/index.js
+++ Clase 4/bloque1/index.js
@@ -53,5 +53,77 @@ nombres.reverse();
 
 console.log(nombres); */
 
-const nombres = ["andres", "camila", "mariano", "andrea", "lucas", "manuel"];
-console.log(nombres.indexOf("lucas"));
+/* const nombres = ["andres", "camila", "mariano", "andrea", "lucas", "manuel"];
+nombres.reverse();
+console.log(nombres);
+console.log(nombres[2]); */
+
+/* const carrito = [];
+carrito.push("televisor");
+
+console.log(carrito); */
+
+/* const nombres = ["andres", "camila", "mariano", "andrea", "lucas", "manuel"];
+
+const eliminarPersona = (nombre) => {
+  let posicion = nombres.indexOf(nombre);
+
+  if(posicion != -1){
+    nombres.splice(posicion, 1);
+  }
+
+  console.log(nombres);
+}
+
+eliminarPersona("mariano"); */
+
+//ARRAYS DE OBJETOS
+/* const productos = [
+  { id: 1, nombre: "camisa", precio: 1000 },
+  { id: 2, nombre: "gorra", precio: 700 },
+  { id: 3, nombre: "pantalon", precio: 1250 },
+  { id: 4, nombre: "zapato", precio: 2000 },
+];
+
+for(const item of productos){
+  console.log(item.nombre);
+  console.log(item.precio);
+} */
+
+/* for (let index = 0; index < productos.length; index++) {
+  const element = productos[index];
+  console.log(element);
+} */
+/* class Producto{
+  constructor(id, nombre, precio){
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+let producto1 = new Producto(1, "camisa", 1000);
+const productos = [
+  new Producto(2, "pantalon", 1500),
+  new Producto(3, "zapato", 700),
+];
+productos.push(producto1);
+console.log(productos); */
+
+/* class Persona{
+  constructor(nombre){
+    this.nombre = nombre;
+  }
+
+  hablar(){
+    console.log("hola soy " + this.nombre);
+  }
+}
+
+let persona = {
+  nombre: "andres",
+}
+console.log(persona);
+
+let persona1 = new Persona("andres")
+console.log(persona1); */
